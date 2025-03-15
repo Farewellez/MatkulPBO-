f7f678bc0d3319360156e3b2c6e208997f96dbf0
justFarewell_ <ahmadzafarell55@gmail.com>
2025-03-15 14:52:13 +0700

    Update OtoMobil_Tugas3.cs

--- PBOTM3/OtoMobil_Tugas3.cs
+++ PBOTM3/OtoMobil_Tugas3.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Reflection.Metadata;
 
 namespace Tugas1_PBO
 {
     // Ini Super Class
-    class OtoMobil
+    class Kendaraan
     {
         // Ini Atribut
         protected string merk;
@@ -13,7 +13,7 @@ namespace Tugas1_PBO
         protected double harga;
 
         // Ini Konstruktor
-        public OtoMobil(string merk,string jenis,int tahun,double harga)
+        public Kendaraan(string merk, string jenis, int tahun, double harga)
         {
             this.merk = merk;
             this.jenis = jenis;
@@ -28,70 +28,26 @@ namespace Tugas1_PBO
         }
     }
     // Ini SubClass
-    class Sedan : OtoMobil
+    class OtoMobil : Kendaraan
     {
         // Ini Atribut
         private int jumlah_kursi;
         private string tipe_transmisi;
 
         // Ini Konstruktor
-        public Sedan(string merk, string jenis, int tahun, double harga,int jumlah_kursi,string tipe_transmisi)
-            : base(merk,jenis,tahun,harga)
+        public OtoMobil(string merk, string jenis, int tahun, double harga, int jumlah_kursi, string tipe_transmisi)
+            : base(merk, jenis, tahun, harga)
         {
             this.jumlah_kursi = jumlah_kursi;
             this.tipe_transmisi = tipe_transmisi;
         }
-        /* Method untuk menampilkan atribut dan output dari SubClass Sedan
-           Override agar subclass bisa mengubah output dari method yang ada di SuperClass OtoMobil*/
+        /* Method untuk menampilkan atribut dan output dari SubClass Otomobil
+           Override agar subclass bisa mengubah output dari method yang ada di SuperClass Kendaraan*/
         public override void info()
         {
             Console.WriteLine();
             base.info();
             Console.WriteLine($"Dengan jumlah kursi sebanyak {jumlah_kursi} dan Tipe Transmisi {tipe_transmisi}");
-        }
-    }
-    class Truk : OtoMobil
-    {
-        // Ini Atribut
-        private double kapasitas_muatan;
-        private int jumlah_roda;
-
-        // Ini Konstruktor
-        public Truk(string merk, string jenis, int tahun, double harga, double kapasitas_muatan, int jumlah_roda)
-            : base(merk, jenis, tahun, harga)
-        {
-            this.kapasitas_muatan = kapasitas_muatan;
-            this.jumlah_roda = jumlah_roda;
-        }
-        /* Method untuk menampilkan atribut dan output dari SubClass Truk
-           Override agar subclass bisa mengubah output dari method yang ada di SuperClass OtoMobil*/
-        public override void info()
-        {
-            Console.WriteLine();
-            base.info();
-            Console.WriteLine($"Dengan Kapasitas Muatan sebanyak {kapasitas_muatan} ton dan Jumlah Roda {jumlah_roda}");
-        }
-    }
-    class SepedaMotor : OtoMobil
-    {
-        // Ini Atribut
-        private string jenis_motor;
-        private double kapasitas_tangki;
-
-        // Ini Konstruktor
-        public SepedaMotor(string merk, string jenis, int tahun, double harga, string jenis_motor, double kapasitas_tangki)
-            : base(merk, jenis, tahun, harga)
-        {
-            this.jenis_motor = jenis_motor;
-            this.kapasitas_tangki = kapasitas_tangki;
-        }
-        /* Method untuk menampilkan atribut dan output dari SubClass SepedaMotor
-           Override agar subclass bisa mengubah output dari method yang ada di SuperClass OtoMobil*/
-        public override void info()
-        {
-            Console.WriteLine();
-            base.info();
-            Console.WriteLine($"Jenis Motornya adalah {jenis_motor} dengan Kapasitas Tangki {kapasitas_tangki} liter");
         }
     }
     // Memulai Program
@@ -105,13 +61,9 @@ namespace Tugas1_PBO
              Atau bisa disebut variable yang mereferensikan new object dari class tertera
              Atau lebih simple nya membuat object baru dari Class terte ntu
              Properti dan metode didefinisikan dari Class yang direferensikan*/
-            Sedan sedan = new Sedan("Toyota", "Sedan", 2021, 350, 5, "Otomatis");
-            Truk truk = new Truk("Hino", "Truk", 2018, 700, 10, 6);
-            SepedaMotor motor = new SepedaMotor("Honda", "Sepeda Motor", 2023, 25, "Matic", 5);
+            OtoMobil sedan = new OtoMobil("Toyota", "Sedan", 2021, 350, 5, "Otomatis");
             // Menampilkan Output dengan cara mengakses method dari Class menggunakan instance yang dibuat
             sedan.info();
-            truk.info();
-            motor.info();
         }
     }
 }
