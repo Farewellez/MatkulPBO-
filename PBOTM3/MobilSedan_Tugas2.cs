a5b4f9fc50a72e2d7960228e440e0173d835cc5c
justFarewell_ <ahmadzafarell55@gmail.com>
2025-03-15 14:43:18 +0700

    Update MobilSedan_Tugas2.cs

--- PBOTM3/MobilSedan_Tugas2.cs
+++ PBOTM3/MobilSedan_Tugas2.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 
 namespace Tugas1_PBO
 {
     // Ini Super Class
-    class MobilSedan
+    class Mobil
     {
         // Ini Atribut
         protected string merk;
@@ -11,7 +11,7 @@ namespace Tugas1_PBO
         protected double harga;
 
         // Ini Konstruktor
-        public MobilSedan(string merk, int tahun, double harga)
+        public Mobil(string merk, int tahun, double harga)
         {
             this.merk = merk;
             this.tahun = tahun;
@@ -25,14 +25,14 @@ namespace Tugas1_PBO
         }
     }
     // Ini Sub Class
-    class MobilSport : MobilSedan
+    class MobilSedan : Mobil
     {
         // Ini Atribut
         private bool turbo;
 
         // Ini Konstruktor
-        public MobilSport(string merk,int tahun,double harga,bool turbo)
-            : base(merk,tahun,harga)
+        public MobilSedan(string merk, int tahun, double harga, bool turbo)
+            : base(merk, tahun, harga)
         {
             this.turbo = turbo;
         }
@@ -45,45 +45,15 @@ namespace Tugas1_PBO
             Console.WriteLine("Mobil mempunyai fitur turbo = " + turbo);
         }
     }
-    // Ini Sub Class Kedua
-    class MobilListrik : MobilSedan
-    {
-        // Ini atribut
-        private double kapasitas_baterai;
-
-        // Ini Konstruktor
-        public MobilListrik(string merk, int tahun, double harga,double kapasitas_baterai)
-            : base(merk,tahun,harga)
-        {
-            this.kapasitas_baterai = kapasitas_baterai;
-        }
-        // Method Local
-        private string battery()
-        {
-            return $"Kapasitas baterai = {kapasitas_baterai} kWh";
-        }
-        // Menampilkan output dari attribut
-        public override void informasi_mobil()
-        {
-            Console.WriteLine();
-            Console.WriteLine($"> Merek: {merk}\n> Tahun keluaran: {tahun}\n> Harga Jual: {harga} M");
-            Console.WriteLine(battery());
-        }
-    }
-    // Class untuk memanggil keseluruhan class dalam diagram class MobilSedan
+    // Class untuk memanggil keseluruhan class dalam diagram class Mobil
     class Program
     {
         // Method yang dijalankan terlebih dahulu
         public static void Main()
         {
             Console.WriteLine("------------Informasi Mobil Sedan------------");
-            MobilSedan mobil = new MobilSedan("Toyota Corolla",2015,250);
-            MobilSport mobil2 = new MobilSport("BMW 320i",2020,900,true);
-            MobilListrik mobil3 = new MobilListrik("Tesla",2022,1,100);
-
+            MobilSedan mobil = new MobilSedan("Toyota Corolla", 2015, 250,false);
             mobil.informasi_mobil();
-            mobil2.informasi_mobil();
-            mobil3.informasi_mobil();
         }
     }
 }
